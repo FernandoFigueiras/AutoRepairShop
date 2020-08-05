381ead1c2179fcc8bf351f77139eacbffb274c8b
Fernando Figueiras <fjafig@gmail.com>
2020-08-05 10:22:32 +0100

    Generic repository

---
+++ AutoRepairShop.Web/Data/Entities/IEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AutoRepairShop.Web.Data.Entities
+{
+    public interface IEntity
+    {
+
+
+        int Id { get; set; }
+
+
+
+        DateTime? CreationDate { get; set; }
+
+
+
+        DateTime? UpdateDate { get; set; }
+
+
+
+        DateTime? InactiveDate { get; set; }
+
+
+
+        bool? IsActive { get; set; }
+    }
+}
